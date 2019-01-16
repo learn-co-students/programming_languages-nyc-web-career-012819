2d8e5069ad7bf42f63a783c3c48435571f9445c4
Kyung Won Park <stomg7969@gmail.com>
2019-01-16 21:38:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |styles, lang_hash|
+    lang_hash.each do |lang, type_hash|
+      type_hash.each do |type, value|
+        if new_hash[lang]
+          new_hash[lang][:style] << styles
+        else
+          new_hash[lang] = {type => value}
+          new_hash[lang][:style] = [styles]
+        end
+      end
+    end
+  end
+  return new_hash
 end

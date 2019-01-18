37c2da0a1acccad63e9a55770d2224845bcd8d4c
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-18 00:50:04 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
+
 def reformat_languages(languages)
-  # your code here
+  langs = {}
+  style = []
+  languages.each do |s, l|
+    style << s
+    l.each do |a, b|
+      langs[a] = b
+    end
+  end
+  langs.each do |lang, attr|
+    if (lang == :ruby || lang == :python || lang == :java)
+      langs[lang][:style] = [style[0]]
+    elsif (lang == :clojure || lang == :erlang || lang == :scala)
+      langs[lang][:style] = [style[1]]
+    else
+      langs[lang][:style] = style
+    end
+  end
+  langs
 end
+
+# 1. store hash in a variable
+# 2. iterate over languages hash
+# 3. for each style put languages inside hash variable
+# 4. for each language put type inside each language
+# 5. for each language put style inside each language as an array
